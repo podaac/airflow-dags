8f2b354634176f3dcbe54c98f51e7480973b45a3
mike-gangl <59702631+mike-gangl@users.noreply.github.com>
2025-05-09 10:08:40 -0700

    Create eddy-process-dag.py
    example of k8s pod operator

---
+++ eddy-process-dag.py
@@ -0,0 +1,27 @@
+import os
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.step_function import StepFunctionStartExecutionOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from datetime import datetime
+
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.secret import Secret
+
+with DAG(
+    dag_id="sar-eddy-detection",
+    schedule_interval=None,
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=["aws", "sar", "eddy", "data-production"],
+) as dag:
+
+    # Get step function input from JSON file
+    k = KubernetesPodOperator(
+      task_id="test_error_message",
+      image="alpine",
+      cmds=["/bin/sh"],
+      arguments=["-c", "echo hello world; echo Custom error > /dev/termination-log; exit 1;"],
+      name="test-error-message",
+      email="airflow@example.com",
+      email_on_failure=True,
+    )
