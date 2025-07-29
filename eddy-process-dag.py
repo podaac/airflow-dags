573d7eff8ea44801e6d0c554c93fb3b9e67aff94
mike-gangl <59702631+mike-gangl@users.noreply.github.com>
2025-07-29 14:56:10 -0700

    Update eddy-process-dag.py

--- eddy-process-dag.py
+++ eddy-process-dag.py
@@ -6,6 +6,9 @@ from datetime import datetime
 
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.providers.cncf.kubernetes.secret import Secret
+from kubernetes.client import models as k8s
+
+
 
 with DAG(
     dag_id="sar-eddy-detection",
@@ -15,10 +18,31 @@ with DAG(
     tags=["aws", "sar", "eddy", "data-production"],
 ) as dag:
 
+
+    volume = k8s.V1Volume(
+        name="test-volume",
+        persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="test-volume"),
+    )
+    volume_mount = k8s.V1VolumeMount(
+        name="test-volume", mount_path="/root/mount_file", sub_path=None, read_only=True
+    )
+
     # Get step function input from JSON file
     k = KubernetesPodOperator(
       task_id="test_sar_eddy_docker",
-      image="gangl/sar-eddy",
+      image="ghcr.io/mike-gangl/podaac-sar-eddy:main",
+      volumes=[
+        k8s_models.V1Volume(
+            name="dshm",
+            empty_dir=k8s.V1EmptyDirVolumeSource(medium="Memory", size_limit="2Gi") # Sets SHM to 2 GiB
+        )
+      ],
+      volume_mounts=[
+        k8s_models.V1VolumeMount(
+            name="dshm",
+            mount_path="/dev/shm"
+        )
+      ],
       #cmds=["/bin/sh"],
       #arguments=["-c", "echo hello world"]
       # name="test-error-message",
