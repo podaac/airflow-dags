04213b2fbc04abed91fe4bcc127a56cb1a2bbb0f
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2025-09-26 12:20:38 -0700

    Fix DAG parse error

--- eddy-process-dag.py
+++ eddy-process-dag.py
@@ -124,8 +124,7 @@ with DAG(
       affinity=affinity,
       container_resources=pod_resources,
       log_events_on_failure=True,
-      cmds=["/bin/sh", "-c"],
-      args=["sh","run_hype.sh"],
+      cmds=["/bin/sh", "run_hype.sh"],
       #arguments=["-c", "echo hello world"]
       # name="test-error-message",
       # email="airflow@example.com",
