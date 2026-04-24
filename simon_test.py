6317a2d792665dcbd06b857a164c326bc08a32ae
Simon Liu <simon.liu@jpl.nasa.gov>
2026-04-24 14:33:57 -0700

    test

--- simon_test.py
+++ simon_test.py
@@ -87,7 +87,7 @@ def has_running_ec2_capacity(min_age_minutes: int = 3) -> bool:
     return False
 
 with DAG(
-    dag_id="simon_dag_test_3",
+    dag_id="simon_dag_test_4",
     schedule=None,
     start_date=datetime(2021, 1, 1),
     tags=["aws", "ecs", "cloud-optimized"],
